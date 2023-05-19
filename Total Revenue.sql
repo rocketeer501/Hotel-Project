e86d6374725328afd2c227bc111758d4856befdb
rocketeer501 <61433845+rocketeer501@users.noreply.github.com>
2023-05-19 19:53:30 -0400

    Update Total Revenue.sql

--- Total Revenue.sql
+++ Total Revenue.sql
@@ -5,7 +5,8 @@ select * from dbo.['2019$']
 union 
 select * from dbo.['2020$'])
 
-select SUM(adr*(1-Discount)*(stays_in_week_nights+stays_in_weekend_nights)+Cost) from hotels
+select SUM((adr*(1-Discount))*(stays_in_weekend_nights+stays_in_week_nights)) as Revenue
+from hotels
 left join dbo.market_segment$
 ON hotels.market_segment=dbo.market_segment$.market_segment
 left join dbo.meal_cost$
