62845fc51ec3722ab0e86b33daa6888c30f45f35
rocketeer501 <61433845+rocketeer501@users.noreply.github.com>
2023-05-19 19:55:20 -0400

    Add files via upload

---
+++ Room Revenue.sql
@@ -0,0 +1,20 @@
+with hotels as (
+select * from dbo.['2018$']
+union
+select * from dbo.['2019$']
+union 
+select * from dbo.['2020$'])
+
+select SUM((adr*(1-Discount))*(stays_in_weekend_nights+stays_in_week_nights)) as Revenue
+from hotels
+left join dbo.market_segment$
+ON hotels.market_segment=dbo.market_segment$.market_segment
+left join dbo.meal_cost$
+ON meal_cost$.meal=hotels.meal
+WHERE is_canceled = 0
+
+
+
+
+
+ 
